e14911e8a423417b8bdaac6844fe3fbe580bfcce
Diegoav87 <diegoabdov@gmail.com>
2020-12-15 19:27:58 -0600

    Fixedm favicon.ico

--- poll_app/urls.py
+++ poll_app/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from django.conf.urls import url
+from django.views.generic import RedirectView
+
 
 
 urlpatterns = [
